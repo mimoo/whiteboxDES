30de3c92a42d19319909cdf216ec23bd57fb5971
David Wong <davwong@emi.u-bordeaux1.fr>
2014-03-10 17:43:47 +0100

    structure

---
+++ src/LtablesGen.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "DES.h"
+
+int main()
+{
+    // take KEY as argument
+
+    // Make subkeys (use DES algorithm)
+
+    // Make Lookuptables in Ltables.c
+
+    return EXIT_SUCCESS;
+}
