9bbc219dff928ae63d7f60308d123d2a9bb376ef
David Wong <davwong@emi.u-bordeaux1.fr>
2014-03-12 15:24:30 +0100

    structure

--- src/WBDES.c
+++ src/WBDES.c
@@ -3,5 +3,14 @@
 
 void rounds(uint64_t *data, int round)
 {
+    uint64_t left_block = 0;
+    uint64_t right_block = 0;
+    uint64_t right_block_temp = 0;
 
+    // 1. Block expansion
+    for(int ii = 0; ii < 48; ii++)
+	addbit(&right_block, *data, (DesExpansion[ii] + 31), ii);
+
+    // 2. 16bits of right_block_temp
+    right_block_temp =
 }
